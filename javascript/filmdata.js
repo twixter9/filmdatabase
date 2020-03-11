14cad475564e9af1745525f226e7b82f7339c12e
Mats <mats.a.j.evensen@gmail.com>
2020-03-11 22:39:16 +0100

    imorve ui and add genre js

---
+++ javascript/filmdata.js
@@ -0,0 +1,37 @@
+$.getJSON('https://api.themoviedb.org/3/trending/all/day?api_key=f5fc7dd9cda2680e5d8b59d03c3bb9ec', function(data) {
+
+    for (i = 0; i < data.results.length; i++) {
+        $(".row").append(
+          
+            '<div class="col-lg-5ths pb-5">' +
+            '<img class="rounded" src="' + 'https://image.tmdb.org/t/p/w185' + data.results[i].poster_path + '" alt="">' +
+            '<h5>' + data.results[i].title + '</h5>' +
+            // '<h6>' + myMap.get(data.results[i].genre_ids[0]) + '</h6>' +
+            '</div>'
+        );
+    }
+    $.getJSON('https://api.themoviedb.org/3/genre/movie/list?api_key=f5fc7dd9cda2680e5d8b59d03c3bb9ec&language=en-US', function getGenre(data) {
+
+        let myMap = new Map()
+    
+        var i;
+        for (i = 0; i < data.genres.length; i++) {
+            myMap.set(data.genres[i].id, data.genres[i].name)
+        }    
+    });
+
+});
+
+
+
+
+
+
+  
+    
+
+
+
+
+
+
